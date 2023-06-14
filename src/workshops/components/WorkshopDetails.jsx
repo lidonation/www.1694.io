239a3203d0bc7ee2da7ecb9a1af5adb7504ce8c3
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-06-14 17:14:37 +0300

    Add the workshop link

--- src/workshops/components/WorkshopDetails.jsx
+++ src/workshops/components/WorkshopDetails.jsx
@@ -79,7 +79,7 @@ function WorkshopDetails({ workshop }) {
                 </dt>
                 <dd>{workshop.locationDescription}</dd>
               </div>
-              <div className="flex gap-x-4 items-center">
+              <div className="flex items-center gap-x-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -94,11 +94,19 @@ function WorkshopDetails({ workshop }) {
                     d="M9.143 17.082a24.248 24.248 0 003.844.148m-3.844-.148a23.856 23.856 0 01-5.455-1.31 8.964 8.964 0 002.3-5.542m3.155 6.852a3 3 0 005.667 1.97m1.965-2.277L21 21m-4.225-4.225a23.81 23.81 0 003.536-1.003A8.967 8.967 0 0118 9.75V9A6 6 0 006.53 6.53m10.245 10.245L6.53 6.53M3 3l3.53 3.53"
                   />
                 </svg>
-                <div className='flex flex-col items-start'>
-                    <span>Start date and time: {workshop.startDateTime}</span>
-                    <span>End date and time: {workshop.endDateTime}</span>
+                <div className="flex flex-col items-start">
+                  <span>Start date and time: {workshop.startDateTime}</span>
+                  <span>End date and time: {workshop.endDateTime}</span>
                 </div>
               </div>
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href={workshop.link}
+                className="text-sm font-semibold text-rose-400 hover:text-rose-600 dark:text-rose-200 dark:hover:text-rose-500 hover:underline"
+              >
+                Click here for more information.
+              </a>
             </dl>
           </div>
         </div>
