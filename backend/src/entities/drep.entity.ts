61d1bfce868232ca3d07aab74c1956fa0738b19d
Darlington <darlington@lidonation.com>
2024-03-22 13:50:38 +0300

    reafactoring code structure

--- backend/src/entities/drep.entity.ts
+++ backend/src/entities/drep.entity.ts
@@ -1,25 +1,25 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity()
-export class Drep{
+export class Drep {
     //auto increment primary key decorator
     @PrimaryGeneratedColumn()
-    id:number
+    id: number
     //Human readable name for the entity
-    @Column({unique:true, nullable:false})
-    name:string
+    @Column({unique: true, nullable: false})
+    name: string
     //More info about the drep
     @Column({})
-    bio:string
+    bio: string
     //Platform statement associated with the user
-    @Column({nullable:false})
-    platform_statement:string
+    @Column({nullable: false})
+    platform_statement: string
     //Information about the user's expertise
     @Column({})
-    expertise:string
+    expertise: string
     //User's perspective within the decentralized system
     @Column({})
-    perspective:string
+    perspective: string
 
     //DB triggers, triggers a function to autorun if a column has been provided in the req body
     //use BeforeInsert decorator
