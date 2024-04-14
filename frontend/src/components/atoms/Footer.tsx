4072416407f3a8dae7a9643eaa67c403a5ca283a
Bewton <mbuto152@gmail.com>
2024-04-14 21:35:59 +0300

    Modified rereouting to include locale. Works for default only

--- frontend/src/components/atoms/Footer.tsx
+++ frontend/src/components/atoms/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="absolute bottom-0 z-0 bg-dark-indigo flex-col items-center justify-center text-white py-2 w-full">
+    <div className="font-poppins absolute bottom-0 z-0 bg-dark-indigo flex-col items-center justify-center text-white py-2 w-[100%]">
       <div className="flex items-center justify-center p-5 mb-12">
         <img src="/sancho-white.svg" alt="Sancho logo" width={'300px'} height={'77px'}/>
       </div>
--- frontend/src/components/atoms/Header.tsx
+++ frontend/src/components/atoms/Header.tsx
@@ -22,8 +22,8 @@ const Header = () => {
       <div className="ml-10 p-3">
         <img src="/sancho-black.svg" alt="Sancho logo" />
       </div>
-      <div className="flex m-5 items-center text-sm font-bold text-nowrap gap-4 md:mr-20 lg:mr-25">
-        <Link href="/dreps" className="text-orange-600">
+      <div className="flex m-5 items-center text-sm font-bold font-poppins text-nowrap gap-4 md:mr-20 lg:mr-25">
+        <Link href="/dreps"  className="text-orange-600">
           Home
         </Link>
         <Link href="/dreps/list">DReps table</Link>
