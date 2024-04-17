71970965eacb59ba05ebc80494eda4614b1f96cc
Mechack 8pro <mechackkobius@gmail.com>
2024-04-17 16:51:31 +0300

    add the correct url to the note:/greps/notes

--- frontend/src/components/atoms/Header.tsx
+++ frontend/src/components/atoms/Header.tsx
@@ -17,20 +17,20 @@ const Header = () => {
       <div className="flex m-5 items-center text-sm font-bold  text-nowrap gap-6 md:mr-20 lg:mr-25">
         <Link
           href="/dreps"
-          onClick={()=>setActiveTab('/dreps')}
+          onClick={() => setActiveTab("/dreps")}
           className={activeTab === "/dreps" ? "text-active" : ""}
         >
           What are DReps
         </Link>
         <Link
           href="/dreps/list"
-          onClick={()=>setActiveTab('/dreps/list')}
+          onClick={() => setActiveTab("/dreps/list")}
           className={activeTab === "/dreps/list" ? "text-active" : ""}
         >
           DRep List
         </Link>
         {/*<Link href="#">Become a DRep</Link>*/}
-        <Link href="#">Notes</Link>
+        <Link href="dreps/notes">Notes</Link>
         <Link href="#">Ecosystem</Link>
         {/*<Link href="#" className="text-blue-800 font-bold">*/}
         {/*  Create profile*/}
