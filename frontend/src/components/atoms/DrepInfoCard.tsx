cc09e54922370ff96ac6da6a9918d489ecceaac8
Darlington Kofa <darlington@lidonation.com>
2024-05-17 23:24:11 +0300

    cleanup

--- frontend/src/components/atoms/DrepInfoCard.tsx
+++ frontend/src/components/atoms/DrepInfoCard.tsx
@@ -15,13 +15,13 @@ interface  DrepInfoCardProps {
 const DrepInfoCard = ({img, title, description, action = null}: DrepInfoCardProps) => {
     return (
         <div
-            className="flex h-full flex-1 w-full flex-col items-start justify-center rounded-lg bg-blue-800 p-5 shadow-lg">
+            className="flex h-full flex-1 w-full flex-col items-start rounded-lg bg-blue-800 p-5 shadow-lg">
             <img src={img} alt={title} width={'60px'} className="mb-3"/>
             <p className="mb-3 text-lg font-extrabold">{title}</p>
             <p className="text-sm font-extralight">{description}</p>
 
             {!!action &&
-                <div className="mt-4">
+                <div className="mt-auto pt-4">
                     <Button sx={{width: 'fit-content'}} variant="contained">
                         <Link href={action.href}>
                             {action.label}
