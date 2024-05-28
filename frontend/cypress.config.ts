2f7a1658953354bd632d0acb5f33b7c51f21fb22
theeurbanlegend <mbuto152@gmail.com>
2024-05-28 18:29:08 +0300

    Fix script

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -5,12 +5,14 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000',
   },
-
+  
   component: {
     devServer: {
       framework: 'next',
       bundler: 'webpack',
     },
+    
   },
 });
