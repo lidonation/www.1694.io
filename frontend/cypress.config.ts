55568c4cf5e3353ddc377eb078a0d2383e240046
etiti <emmanueltiti370@gmail.com>
2024-07-27 15:21:30 +0300

    cypress

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  chromeWebSecurity: false,
+  retries: 2,
+  defaultCommandTimeout: 20000,
+  watchForFileChanges: false,
+  video: true,
+  screenshots: false,
+  videosFolder: 'frontend/cypress/videos',
+  screenshotsFolder: 'frontend/cypress/screenshots',
+  fixturesFolder: 'frontend/cypress/fixture',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
