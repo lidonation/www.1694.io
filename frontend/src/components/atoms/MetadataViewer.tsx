27d5544fe5d050e8d3c9bd06322a375f51906ee3
theeurbanlegend <mbuto152@gmail.com>
2024-08-08 14:06:44 +0300

    Update, format fix

--- frontend/src/components/atoms/MetadataViewer.tsx
+++ frontend/src/components/atoms/MetadataViewer.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const MetadataViewer = ({ isMetadataLoading, metadataError, metadata, metadataUrl }: { isMetadataLoading: boolean, metadataError: any, metadata: any, metadataUrl: string }) => {
-
+const MetadataViewer = ({
+  isMetadataLoading,
+  metadataError,
+  metadata,
+  metadataUrl,
+}: {
+  isMetadataLoading: boolean;
+  metadataError: any;
+  metadata: any;
+  metadataUrl: string;
+}) => {
   const renderValue = (value: any) => {
     if (typeof value === 'object' && value['@value']) {
       return value['@value'];
@@ -15,28 +24,29 @@ const MetadataViewer = ({ isMetadataLoading, metadataError, metadata, metadataUr
 
   const renderContent = () => {
     if (isMetadataLoading) {
-      return  <p className='text-sm'>Loading...</p>;
+      return <p className="text-sm">Loading...</p>;
     }
 
     if (metadataError) {
-      return <p className='text-sm'>{metadataError + '\n' + metadataUrl}</p>;
+      return <p className="text-sm">{metadataError + '\n' + metadataUrl}</p>;
     }
 
     if (!metadata || !metadata.body) {
-      return <p className='text-sm'>No metadata found</p>;
+      return <p className="text-sm">No metadata found</p>;
     }
 
     return Object.entries(metadata.body).map(([key, value]: any[]) => {
       const valueString = renderValue(value);
       return (
-        <p key={key}>
-          {capitalizeFirstLetter(key)}: {valueString}
-        </p>
+        <div key={key} className="flex items-center gap-1 text-sm">
+          <p className="font-semibold">{capitalizeFirstLetter(key)}</p>........
+          <p>{valueString}</p>
+        </div>
       );
     });
   };
 
-  return <div>{ renderContent() }</div>;
+  return <div className='flex flex-col gap-2'>{renderContent()}</div>;
 };
 
 export default MetadataViewer;
--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -3,43 +3,12 @@ export const networkIds = {
   SANCHONET: 0, 
   PREVIEW: 0,
 };
+
 export const CONFIGURED_NETWORK_ID = Number(process.env.NEXT_PUBLIC_NETWORK_ID);
-function getBaseServerUrl() {
-  if (CONFIGURED_NETWORK_ID === networkIds.MAINNET) {
-    return process.env.NEXT_PUBLIC_BASE_SERVER_MAINNET_URL;
-  }
-  if (CONFIGURED_NETWORK_ID === networkIds.PREVIEW) {
-    return process.env.NEXT_PUBLIC_BASE_SERVER_PREVIEW_URL;
-  }
-  if (CONFIGURED_NETWORK_ID === networkIds.SANCHONET) {
-    return process.env.NEXT_PUBLIC_BASE_SERVER_SANCHO_URL;
-  }
-}
-function getBaseExplorer() {
-  if (CONFIGURED_NETWORK_ID === networkIds.MAINNET) {
-    return 'https://cexplorer.io/';
-  }
-  if (CONFIGURED_NETWORK_ID === networkIds.PREVIEW) {
-    return 'https://preview.cexplorer.io/';
-  }
-  if (CONFIGURED_NETWORK_ID === networkIds.SANCHONET) {
-    return 'https://sancho.cexplorer.io';
-  }
-}
-function getBaseGovTool() {
-  if (CONFIGURED_NETWORK_ID === networkIds.MAINNET) {
-    return 'https://govtool.io/'; // coming soon
-  }
-  if (CONFIGURED_NETWORK_ID === networkIds.PREVIEW) {
-    return 'https://preview.gov.tools';
-  }
-  if (CONFIGURED_NETWORK_ID === networkIds.SANCHONET) {
-    return 'https://sanchogov.tools';
-  }
-}
+
 export const urls = {
   baseProdUrl: '',
-  baseServerUrl: getBaseServerUrl(),
-  govToolUrl: getBaseGovTool(),
-  cexplorerUrl: getBaseExplorer(),
+  baseServerUrl: process.env.NEXT_PUBLIC_BASE_API_URL || '',
+  govToolUrl: process.env.NEXT_PUBLIC_BASE_GOVTOOL_URL || '',
+  cexplorerUrl: process.env.NEXT_PUBLIC_BASE_CEXPLORER_URL || '',
 };
