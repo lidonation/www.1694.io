7ecf48ad8e0293ba0bdd10b3e43399cfef94c5b7
Darlington Kofa <darlington@lidonation.com>
2024-08-09 17:27:27 +0300

    cleanup environment variables

--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -1,8 +1,7 @@
-export const CONFIGURED_NETWORK_ID = Number(process.env.NETWORK_ID);
+export const CONFIGURED_NETWORK_ID = Number(process.env.NEXT_PUBLIC_NETWORK_ID);
 
 export const urls = {
   baseServerUrl: process.env.NEXT_PUBLIC_BASE_URL_API || '',
   govToolUrl: process.env.NEXT_PUBLIC_BASE_URL_GOVTOOL || '',
   cexplorerUrl: process.env.NEXT_PUBLIC_BASE_URL_EXPLORER || '',
 };
-console.log(urls);
