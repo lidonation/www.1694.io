42d99e7b4347bb535c7fa0ce8cbe58394fb98639
theeurbanlegend <mbuto152@gmail.com>
2024-09-05 23:38:22 +0300

    added loaders

--- frontend/src/app/[locale]/dreps/[drepid]/delegators/page.tsx
+++ frontend/src/app/[locale]/dreps/[drepid]/delegators/page.tsx
@@ -1,13 +1,19 @@
 'use client';
+import Loading from '@/app/[locale]/loading';
 import DrepProfileMetrics from '@/components/molecules/DrepProfileMetrics';
 import { useGetSingleDRepQuery } from '@/hooks/useGetSingleDRepQuery';
 import { useParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 const DelegatorsPage = () => {
   const { drepid } = useParams();
   const { dRep } = useGetSingleDRepQuery(drepid);
 
-  return <DrepProfileMetrics drepMetrics={dRep} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <DrepProfileMetrics drepMetrics={dRep} />;
+    </Suspense>
+  );
 };
 
 export default DelegatorsPage;
--- frontend/src/app/[locale]/dreps/[drepid]/page.tsx
+++ frontend/src/app/[locale]/dreps/[drepid]/page.tsx
@@ -5,26 +5,29 @@ import DrepTimeline from '@/components/molecules/DrepTimeline';
 import { useGetSingleDRepQuery } from '@/hooks/useGetSingleDRepQuery';
 import { useParams } from 'next/navigation';
 import DrepClaimProfileCard from '@/components/atoms/DrepClaimProfileCard';
+import Loading from '../../loading';
 
 const page = () => {
   const { drepid } = useParams();
   const { dRep, isDRepLoading } = useGetSingleDRepQuery(drepid);
 
   return (
-    <div className="flex flex-col lg:flex-row">
-      <div className="lg:sticky lg:top-10 lg:w-[30%] lg:self-start lg:!scroll-smooth">
-        {dRep?.drep_id ? (
-          <DrepProfileCard drep={dRep} state={isDRepLoading} />
-        ) : (
-          <DrepClaimProfileCard drep={dRep} state={isDRepLoading} />
-        )}
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:sticky lg:top-10 lg:w-[30%] lg:self-start lg:!scroll-smooth">
+          {dRep?.drep_id ? (
+            <DrepProfileCard drep={dRep} state={isDRepLoading} />
+          ) : (
+            <DrepClaimProfileCard drep={dRep} state={isDRepLoading} />
+          )}
+        </div>
+        <div className="lg:w-[70%]">
+          <Suspense>
+            <DrepTimeline cexplorerDetails={dRep?.cexplorerDetails} />
+          </Suspense>
+        </div>
       </div>
-      <div className="lg:w-[70%]">
-        <Suspense>
-          <DrepTimeline cexplorerDetails={dRep?.cexplorerDetails} />
-        </Suspense>
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
