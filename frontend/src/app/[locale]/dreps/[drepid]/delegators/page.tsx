d808232ad1028cb0d432467c8a2509dee4ee1347
theeurbanlegend <mbuto152@gmail.com>
2024-09-09 15:12:32 +0300

    fixes, cleanup, copy full page loader to drep profile layout

--- frontend/src/app/[locale]/dreps/[drepid]/delegators/page.tsx
+++ frontend/src/app/[locale]/dreps/[drepid]/delegators/page.tsx
@@ -1,19 +1,12 @@
 'use client';
-import Loading from '@/app/[locale]/loading';
 import DrepProfileMetrics from '@/components/molecules/DrepProfileMetrics';
 import { useGetSingleDRepQuery } from '@/hooks/useGetSingleDRepQuery';
 import { useParams } from 'next/navigation';
-import { Suspense } from 'react';
 
 const DelegatorsPage = () => {
-  const { drepIdd } = useParams();
-  const { dRep } = useGetSingleDRepQuery(drepIdd);
-
-  return (
-    <Suspense fallback={<Loading />}>
-      <DrepProfileMetrics drepMetrics={dRep} />;
-    </Suspense>
-  );
+  const { drepid } = useParams();
+  const { dRep } = useGetSingleDRepQuery(drepid);
+  return <DrepProfileMetrics drepMetrics={dRep} />;
 };
 
 export default DelegatorsPage;
--- frontend/src/app/[locale]/dreps/[drepid]/page.tsx
+++ frontend/src/app/[locale]/dreps/[drepid]/page.tsx
@@ -5,14 +5,12 @@ import DrepTimeline from '@/components/molecules/DrepTimeline';
 import { useGetSingleDRepQuery } from '@/hooks/useGetSingleDRepQuery';
 import { useParams } from 'next/navigation';
 import DrepClaimProfileCard from '@/components/atoms/DrepClaimProfileCard';
-import Loading from '../../loading';
 
 const page = () => {
   const { drepid } = useParams();
   const { dRep, isDRepLoading } = useGetSingleDRepQuery(drepid);
 
   return (
-    <Suspense fallback={<Loading />}>
       <div className="flex flex-col lg:flex-row">
         <div className="lg:sticky lg:top-10 lg:w-[30%] lg:self-start lg:!scroll-smooth">
           {dRep?.drep_id ? (
@@ -27,7 +25,6 @@ const page = () => {
           </Suspense>
         </div>
       </div>
-    </Suspense>
   );
 };
 
