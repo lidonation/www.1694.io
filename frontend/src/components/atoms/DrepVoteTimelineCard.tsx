05019e6fc089184e0765281b2d6cd04318fc1046
cjoan <cindyjoan2000@gmail.com>
2024-09-27 22:49:23 +0300

    Dropdown removal from governance action preview

--- frontend/src/components/atoms/DrepVoteTimelineCard.tsx
+++ frontend/src/components/atoms/DrepVoteTimelineCard.tsx
@@ -4,8 +4,6 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Box } from "@mui/material";
-import { MenuItem, Select, InputLabel, FormControl } from '@mui/material';
-import { styled } from '@mui/system';
 import axios from 'axios';
 
 const VoteStatusChip = ({ date, vote }: { date: string, vote: string }) => {
@@ -22,29 +20,15 @@ const VoteStatusChip = ({ date, vote }: { date: string, vote: string }) => {
 const DrepVoteTimelineCard = ({ item }: { item: any }) => {
   const [govActionName, setGovActionName] = useState('')
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get(item.url).
-    then((response)=>{
-      setGovActionName(response?.data?.body?.title)
-    }).catch((error)=>{
-      console.log(error)
-    })
+      then((response) => {
+        setGovActionName(response?.data?.body?.title)
+      }).catch((error) => {
+        console.log(error)
+      })
   }, [govActionName])
 
-  const StyledInputLabel = styled(InputLabel)(() => ({
-    position: 'absolute',
-    top: '50%',
-    transform: 'translateY(-50%)',
-    transition: 'top 0.3s, transform 0.3s',
-    '&.MuiInputLabel-shrink': {
-      top: 0,
-      transform: 'translateY(-90%)',
-    },
-    left: '20px',
-    fontSize: '14px'
-  }));
-
-  
   return (
     <Box
       id="epoch-card"
@@ -52,13 +36,13 @@ const DrepVoteTimelineCard = ({ item }: { item: any }) => {
     >
       <VoteStatusChip date={item?.time_voted} vote={item?.vote} />
       <hr />
-      <Box className="flex max-w-52 flex-col gap-1">
+      <Box className="flex max-w-52 flex-col">
         <p className="text-lg font-bold">
           For {item?.description?.tag || null}
         </p>
-        <p className="text-sm">Governance Action Name:</p>
+        <p className="text-sm mt-2">Governance Action Name:</p>
         <p className="text-sm font-bold">{govActionName}</p>
-        <p className="text-sm">Governance Action ID:</p>
+        <p className="text-sm mt-2">Governance Action ID:</p>
         <div className="flex w-fit items-center gap-1 rounded-full border px-3 py-1 text-sm">
           <p>
             {convertString(item?.gov_action_proposal_id + '#0', true) || null}
@@ -74,33 +58,23 @@ const DrepVoteTimelineCard = ({ item }: { item: any }) => {
           </CopyToClipboard>
         </div>
       </Box>
-      <FormControl style={{marginTop: '10px', width: '90%', marginBottom: '10px'}}>
-      <StyledInputLabel id="demo-simple-select-label">View Governance Action</StyledInputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          label="View Governance Action"
+      <Box className="flex max-w-52 flex-col gap-1">
+        <p className="text-sm">View Governance Action:</p>
+        <Link
+          href={`${urls.govToolUrl}/governance_actions/${item?.gov_action_proposal_id}#0`}
+          target="_blank"
+          className="text-blue-800 text-sm"
         >
-          <MenuItem>
-            <Link
-              href={`${urls.govToolUrl}/governance_actions/${item?.gov_action_proposal_id}#0`}
-              target="_blank"
-              className="text-blue-800"
-            >
-              Cardano Govtool
-            </Link>
-          </MenuItem>
-          <MenuItem>
-            <Link
-              href={`${urls.adaStatusUrl}/governances/${item?.gov_action_proposal_id}`}
-              target="_blank"
-              className="text-blue-800"
-            >
-              Ada Status
-            </Link>
-          </MenuItem>
-        </Select>
-      </FormControl>
+          Cardano Govtool
+        </Link>
+        <Link
+          href={`${urls.adaStatusUrl}/governances/${item?.gov_action_proposal_id}`}
+          target="_blank"
+          className="text-blue-800 text-sm mb-2"
+        >
+          Ada Status
+        </Link>
+      </Box>
     </Box>
   );
 };
