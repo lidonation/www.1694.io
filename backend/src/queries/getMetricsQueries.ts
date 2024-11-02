fd6ccf76c478fcb5400e750b6c2dbe66c29c72a0
theeurbanlegend <mbuto152@gmail.com>
2024-11-02 23:06:03 +0300

    Updated query for getting active dreps

--- backend/src/queries/getMetricsQueries.ts
+++ backend/src/queries/getMetricsQueries.ts
@@ -49,11 +49,24 @@ WITH latest_epoch AS (
     SELECT MAX(epoch_no) AS latest_epoch_no
     FROM block
 ),
+latest_registration AS (
+    SELECT
+        drep_hash_id,
+        deposit,
+        ROW_NUMBER() OVER (PARTITION BY drep_hash_id ORDER BY id DESC) AS rn
+    FROM
+        drep_registration
+),
 active_dreps AS (
     -- Select distinct hash_ids where active_until is greater than the latest epoch
     SELECT DISTINCT dd.hash_id
     FROM drep_distr dd
     JOIN latest_epoch le ON dd.active_until > le.latest_epoch_no
+    LEFT JOIN
+        latest_registration r ON dd.hash_id = r.drep_hash_id
+    WHERE
+        (r.rn = 1 AND (r.deposit = 500000000 OR r.deposit IS NULL))
+        OR r.drep_hash_id IS NULL  -- Include DReps with no registration entry
 )
 SELECT
     COUNT(*) AS total_active_dreps
