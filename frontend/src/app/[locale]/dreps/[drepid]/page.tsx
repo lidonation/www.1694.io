d560f4d53d40adae8637cc9b4d209284f0ebdbdb
theeurbanlegend <mbuto152@gmail.com>
2025-02-11 19:59:54 +0300

    fix attempt

--- frontend/src/app/[locale]/dreps/[drepid]/page.tsx
+++ frontend/src/app/[locale]/dreps/[drepid]/page.tsx
@@ -5,26 +5,39 @@ import DrepTimeline from '@/components/molecules/DrepTimeline';
 import { useGetSingleDRepQuery } from '@/hooks/useGetSingleDRepQuery';
 import { useParams } from 'next/navigation';
 import DrepClaimProfileCard from '@/components/atoms/DrepClaimProfileCard';
+import Loading from '../loading';
 
 const page = () => {
   const { drepid } = useParams();
   const { dRep, isDRepLoading } = useGetSingleDRepQuery(drepid.toString());
 
+  if (isDRepLoading) {
+    return <Loading />;
+  }
+
   return (
-      <div className="flex flex-col lg:flex-row">
-        <div className="lg:sticky lg:top-10 lg:w-[30%] lg:self-start lg:!scroll-smooth">
-          {dRep?.drep_id ? (
-            <DrepProfileCard drep={dRep} voterId={drepid.toString()} loading={isDRepLoading} />
-          ) : (
-            <DrepClaimProfileCard drep={dRep} voterId={drepid.toString()} loading={isDRepLoading} />
-          )}
-        </div>
-        <div className="lg:w-[70%]">
-          <Suspense>
-            <DrepTimeline drep={dRep} />
-          </Suspense>
-        </div>
+    <div className="flex flex-col lg:flex-row">
+      <div className="lg:sticky lg:top-10 lg:w-[30%] lg:self-start lg:!scroll-smooth">
+        {dRep?.drep_id ? (
+          <DrepProfileCard
+            drep={dRep}
+            voterId={drepid.toString()}
+            loading={isDRepLoading}
+          />
+        ) : (
+          <DrepClaimProfileCard
+            drep={dRep}
+            voterId={drepid.toString()}
+            loading={isDRepLoading}
+          />
+        )}
       </div>
+      <div className="lg:w-[70%]">
+        <Suspense>
+          <DrepTimeline drep={dRep} />
+        </Suspense>
+      </div>
+    </div>
   );
 };
 
