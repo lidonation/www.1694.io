815eef5facdb367f29180886e16b71faff98187d
Darlington Kofa <darlington@lidonation.com>
2025-04-17 22:17:27 +0300

    chore: cleanup budget proposal metrics

--- frontend/src/services/requests/getProposalMetrics.ts
+++ frontend/src/services/requests/getProposalMetrics.ts
@@ -1,6 +1,8 @@
 import axiosInstance from '../axiosInstance';
 
 export const getAllMetrics = async (search?: string) => {
-  const response = await axiosInstance.get(`metrics?s=${encodeURIComponent(search)}`);  
+  let queryParams = typeof  search != "undefined" ? `?s=${encodeURIComponent(search)}` : '';
+
+  const response = await axiosInstance.get(`metrics${queryParams}`);
   return response.data;
 };
