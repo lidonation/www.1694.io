dd213ee0d6af4c8ce95460fe8b3d6253070eaae3
Bewton Maina <mbuto152@gmail.com>
2025-04-18 14:52:43 +0000

    refactor(auth-flow): add login-file as a login option

---
+++ frontend/src/hooks/useVerifyLoginFileSignatures.ts
@@ -0,0 +1,23 @@
+import {
+  verifyLoginFileSigs,
+  VerifyLoginFileSigsPayloadRequest,
+} from '@/services/requests/verifyLoginFileSigs';
+import { useMutation } from 'react-query';
+
+export const useVerifyLoginFileSignatures = () => {
+  const { data, isLoading, isError, mutateAsync } = useMutation(
+    async ({ signature, vkey }: VerifyLoginFileSigsPayloadRequest) => {
+      return await verifyLoginFileSigs({
+        signature,
+        vkey,
+      });
+    },
+  );
+
+  return {
+    isVerifying: isLoading,
+    isError,
+    loginVerification: data,
+    verifyLoginFileSigs: mutateAsync,
+  };
+};
---
+++ frontend/src/services/requests/verifyLoginFileSigs.ts
@@ -0,0 +1,22 @@
+import axiosInstance from '../axiosInstance';
+
+export interface VerifyLoginFileSigsPayloadRequest {
+  signature: string;
+  vkey: string;
+}
+
+export interface VerifyLoginFileSigsPayloadResponse {
+  workMode: 'verify-cip30';
+  payloadResultMatch: boolean;
+  publicKeyHex: string;
+  payloadDataHex: string;
+  signature: string;
+  publicKey: string;
+}
+
+export const verifyLoginFileSigs = async (
+  payload: VerifyLoginFileSigsPayloadRequest,
+) => {
+  const response = await axiosInstance.post('auth/login/verify-sigs', {payload});
+  return response.data as VerifyLoginFileSigsPayloadResponse;
+};
