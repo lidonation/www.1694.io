7066540791b16c18868afbff9edc3c778f6a79a3
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-04-26 16:16:32 +0300

    debug failing govtool endpoint

--- frontend/src/services/requests/getDRepVotingPowerList.ts
+++ frontend/src/services/requests/getDRepVotingPowerList.ts
@@ -5,8 +5,11 @@ export const getDRepVotingPowerList = async (identifiers: string[]) => {
   const params = new URLSearchParams();
   identifiers.forEach((id: string) => params.append('identifiers', id));
 
+  console.log(urls.govToolApiUrl);
+  
   const response = await axiosInstance.get(
-    `${urls.govToolApiUrl}/drep/voting-power-list?${params.toString()}`,
+    `/drep/voting-power-list?${params.toString()}`,
+    { baseURL: urls.govToolApiUrl },
   );
 
   return response.data || [];
