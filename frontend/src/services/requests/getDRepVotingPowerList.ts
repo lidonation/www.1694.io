dca2ebc1a5fc72c5c48e0075f466fa2bdf0d916f
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-04-26 16:54:51 +0300

    fix: move govtool api call to the backend

--- frontend/src/services/requests/getDRepVotingPowerList.ts
+++ frontend/src/services/requests/getDRepVotingPowerList.ts
@@ -1,15 +1,11 @@
-import { urls } from '@/constants';
 import axiosInstance from '../axiosInstance';
 
 export const getDRepVotingPowerList = async (identifiers: string[]) => {
   const params = new URLSearchParams();
   identifiers.forEach((id: string) => params.append('identifiers', id));
 
-  console.log(urls.govToolApiUrl);
-  
   const response = await axiosInstance.get(
-    `/drep/voting-power-list?${params.toString()}`,
-    { baseURL: urls.govToolApiUrl },
+    `/dreps/voting-power-list?${params.toString()}`,
   );
 
   return response.data || [];
