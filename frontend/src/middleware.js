1b7d35b61f126be6d3962db3c658e70e3568674f
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-04-26 21:12:54 +0300

    update middleware runtime to nodejs

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -2,15 +2,14 @@ import createMiddleware from 'next-intl/middleware';
 import { locales } from './constants';
 import { NextResponse } from 'next/server';
 
-// Setup next-intl middleware
 const intlMiddleware = createMiddleware({
   locales: locales.variants,
   defaultLocale: locales.defaultLocale,
 });
 
-// Define which paths middleware should apply to
 export const config = {
   matcher: ['/((?!_next).*)'],
+  runtime: 'nodejs'
 };
 
 function getLocale() {
@@ -20,16 +19,13 @@ function getLocale() {
 export function middleware(request) {
   const { pathname } = request.nextUrl;
 
-  // Skip serving assets like .svg, .png, .ico
   if (pathname.match(/\.(svg|png|ico)$/)) {
     return NextResponse.next();
   }
 
-  // Run the internationalization middleware
   const intlResponse = intlMiddleware(request);
   if (intlResponse) return intlResponse;
 
-  // If no locale is found in the path, redirect to default locale
   const pathnameHasLocale = locales.variants.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
   );
