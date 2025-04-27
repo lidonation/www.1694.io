0a206cd6f0ad7f380da5822e8517da1282dd2d17
Darlington Kofa <darlington@lidonation.com>
2025-04-27 09:42:39 +0300

    chore: test

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -9,7 +9,7 @@ const intlMiddleware = createMiddleware({
 
 export const config = {
   matcher: ['/((?!_next).*)'],
-  runtime: 'edge',
+  runtime: 'experimental-edge',
 };
 
 function getLocale() {
