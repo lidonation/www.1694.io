932257d10b2f2679e6506843ac7670c8dfbac615
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-04-28 13:39:25 +0300

    use absolute import for messages

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,22 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path';
 
 const withNextIntl = createNextIntlPlugin({
   experimental: {
-    createMessagesDeclaration : ["./messages/en.json", "./messages/en.json"]
+    createMessagesDeclaration: ["./messages/en.json", "./messages/en.json"]
   }
 });
 
 const config = {
   output: 'standalone',
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@messages': path.resolve('./messages'),
+    };
+    return config;
+  },
 };
 
 export default withNextIntl(config);
