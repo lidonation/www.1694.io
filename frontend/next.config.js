52ccc03e6f5f666b7738261cd40ce0ba6a85a0a3
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-04-28 17:13:46 +0300

    clean up

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,29 +3,19 @@ import path from 'path';
 
 const withNextIntl = createNextIntlPlugin({
   experimental: {
-    createMessagesDeclaration: ["./messages/en.json", "./messages/en.json"]
-  }
+    createMessagesDeclaration: ['./messages/en.json', './messages/en.json'],
+  },
 });
 
 const config = {
   output: 'standalone',
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@messages': path.resolve('./messages'),
       'rdf-canonize-native': false,
     };
-
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        crypto: false,
-      };
-    }
-
     return config;
   },
 };
