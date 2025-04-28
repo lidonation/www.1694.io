4c32eea2ac3b8d2aa818c7e5980a94e7f9a0f139
Emmanuel Mutisya <emmanuelmutisya254@gmail.com>
2025-04-28 14:07:40 +0300

    use file system to pull in language files

--- frontend/src/i18n/request.ts
+++ frontend/src/i18n/request.ts
@@ -1,16 +1,20 @@
-import {getRequestConfig} from 'next-intl/server';
-import {hasLocale} from 'next-intl';
-import {routing} from './routing';
- 
-export default getRequestConfig(async ({requestLocale}) => {
-  // Typically corresponds to the `[locale]` segment
+import { getRequestConfig } from 'next-intl/server';
+import { hasLocale } from 'next-intl';
+import { routing } from './routing';
+import path from 'path';
+import fs from 'fs';
+
+export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
- 
+
+  const filePath = path.join(process.cwd(), 'messages', `${locale}.json`);
+  const messages = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
   return {
     locale,
-    messages: (await import(`@messages/${locale}.json`)).default
+    messages,
   };
 });
