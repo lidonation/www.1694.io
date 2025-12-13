bc70eb2ba45453f42a558f6b81ba7a22efcbeb8a
etiti <emmanueltiti370@gmail.com>
2025-12-13 23:27:53 +0300

    feat: update CI configuration and enhance frontend settings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,11 @@ const path = require('path');
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 const sassOptions = {
   includePaths: [path.join(__dirname, 'assets/styles')],
