902012d8016e70342fbb9cc4df4eaea3a25aad2f
etiti <emmanueltiti370@gmail.com>
2025-12-23 21:06:29 +0300

    fix(middleware): simplify locale handling and improve asset matching in middleware

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -2,27 +2,20 @@ import createMiddleware from 'next-intl/middleware';
 import { locales } from './constants';
 import { NextResponse } from 'next/server';
 
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
   locales: locales.variants,
   defaultLocale: locales.defaultLocale,
 });
 
-export const config = {
-  matcher: ['/((?!_next).*)'],
-};
+export default function middleware(request) {
+  if (request.nextUrl.pathname.match(/\.(svg|png|ico|css|js)$/)) {
+    return NextResponse.next();
+  }
 
-export function middleware(request) {
-  // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl;
-  const pathnameHasLocale = locales.variants.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
-  );
 
-  if (pathnameHasLocale) return NextResponse.next();
-  if (pathname.match(/\.(svg|png|ico)$/)) return NextResponse.next();
-  // Redirect if there is no locale
-  const locale = locales.defaultLocale;
-
-  request.nextUrl.pathname = `/${locale}${pathname}`;
-  return NextResponse.redirect(request.nextUrl);
+  return intlMiddleware(request);
 }
+
+export const config = {
+  matcher: ['/((?!_next|api).*)'],
+};
