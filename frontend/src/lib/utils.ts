9153f5a450c07f9d17d25a855d0d7a0162b03e8a
etiti <emmanueltiti370@gmail.com>
2026-01-12 21:37:15 +0300

    chore: cleanup

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -113,15 +113,7 @@ export const formatNumberTimeToReadable = (time: number) => {
     time = parseInt(time);
   };
   
-  if (!time || isNaN(new Date(time).getTime())) {
-    return '...';
-  }
-  console.log(new Date(time).toLocaleString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }));
-  
+
   return new Date(time).toLocaleString('en-US', {
     year: 'numeric',
     month: 'long',
